cb1e561d8d57de7953b2a4211f33fd8982da26ba
saranya <saranyasr3223@gmail.com>
2024-08-07 12:56:40 +0530

    created a git hub sample project

---
+++ GithubSample/src/javaproject/Githubaddition.java
@@ -0,0 +1,14 @@
+package javaproject;
+
+public class Githubaddition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b, sum;
+		a = 10;
+		b = 20;
+		sum = a + b;
+		System.out.println("sum is:" + sum);
+	}
+
+}
